bf22cbe5f924afd8d7531cb1ee4951ece8eebff9
mayur2004Id <gupta.mayur@kalvium.community>
2024-03-06 16:54:29 +0530

    numeron

--- game.js
+++ game.js
@@ -1,5 +1,81 @@
-// Iteration 2: Generate 2 random number and display it on the screen
+// Iteration 2: Generate 2 random numbers and display them on the screen
+const firstNumberElement = document.getElementById('number1');
+const secondNumberElement = document.getElementById('number2');
+let firstNumber = Math.round(Math.random() * 100);
+firstNumberElement.innerHTML = firstNumber;
 
-// Iteration 3: Make the options button functional
+let secondNumber = Math.round(Math.random() * 100);
+secondNumberElement.innerHTML = secondNumber;
+
+// Iteration 3: Make the options buttons functional
+const greaterThanButton = document.getElementById('greater-than');
+const equalToButton = document.getElementById('equal-to');
+const lesserThanButton = document.getElementById('lesser-than');
+
+let score = 0;
+
+greaterThanButton.onclick = () => {
+    if (firstNumber > secondNumber) {
+        score++;
+        resetTimer(timerId);
+    } else {
+        location.href = "./gameover.html";
+    }
+    firstNumber = Math.round(Math.random() * 100);
+    firstNumberElement.innerHTML = firstNumber;
+    console.log(score);
+    secondNumber = Math.round(Math.random() * 100);
+    secondNumberElement.innerHTML = secondNumber;
+}
+
+equalToButton.onclick = () => {
+    if (firstNumber === secondNumber) {
+        score++;
+        resetTimer(timerId);
+    } else {
+        location.href = "./gameover.html";
+    }
+    firstNumber = Math.round(Math.random() * 100);
+    firstNumberElement.innerHTML = firstNumber;
+    console.log(score);
+    secondNumber = Math.round(Math.random() * 100);
+    secondNumberElement.innerHTML = secondNumber;
+}
+
+lesserThanButton.onclick = () => {
+    if (firstNumber < secondNumber) {
+        score++;
+        resetTimer(timerId);
+    } else {
+        location.href = "./gameover.html";
+    }
+    firstNumber = Math.round(Math.random() * 100);
+    firstNumberElement.innerHTML = firstNumber;
+    console.log(score);
+    secondNumber = Math.round(Math.random() * 100);
+    secondNumberElement.innerHTML = secondNumber;
+}
 
 // Iteration 4: Build a timer for the game
+let remainingTime = 5;
+const timerElement = document.getElementById("timer");
+let timerId;
+
+function startTimer() {
+    remainingTime = 5;
+    timerElement.innerHTML = remainingTime;
+    timerId = setInterval(() => {
+        remainingTime--;
+        if (remainingTime === 0) {
+            location.href = "./gameover.html"
+        }
+        timerElement.innerHTML = remainingTime;
+    }, 1000);
+    localStorage.setItem("score", score);
+}
+
+function resetTimer(intervalId) {
+    clearInterval(intervalId);
+    startTimer();
+}
+startTimer();
--- score.js
+++ score.js
@@ -1,1 +1,9 @@
 // Iteration 5: Store the player score and display it on the game over screen
+let score = localStorage.getItem("score");
+
+var scorecard = document.getElementById("score-board").innerHTML = score;
+
+const PlayButton = document.getElementById("play-again-button");
+PlayButton.onclick = function(){
+    location.href = "./index.html";
+}
